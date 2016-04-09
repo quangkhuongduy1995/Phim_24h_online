6ed2f97283346143da601cfd2e646b14e72b645d
quangkhuongduy <quangkhuongduy@outlook.com.vn>
2016-04-09 23:53:01 +0700

    Initial Commit

---
+++ Phim 24hTests/Phim_24hTests.swift
@@ -0,0 +1,36 @@
+//
+//  Phim_24hTests.swift
+//  Phim 24hTests
+//
+//  Created by Quảng Khương Duy on 4/9/16.
+//  Copyright © 2016 Com.QuangKhuongDuy.Phim24h. All rights reserved.
+//
+
+import XCTest
+@testable import Phim_24h
+
+class Phim_24hTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
